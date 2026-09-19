c491b28aaa2bf21a84e7a65d7ae9debd9c80912c
Daniel Barrot <byarrley@gmail.com>
2026-09-19 15:31:04 -0400

    implement initial solution

--- Ch02/challenge/activity.go
+++ Ch02/challenge/activity.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -18,11 +19,34 @@ import (
 	* description: ...long
 */
 
+const maxLenUser = 16
+const maxLenDescription = 1024
+
 type Activity struct {
 	User        string    `json:"user"`
 	StartTime   time.Time `json:"start_time"`
 	EndTime     time.Time `json:"end_time"`
 	Description string    `json:"description"`
+}
+
+func (a *Activity) Validate() error {
+	//We could check that all fields are present, and populated.
+	if l := len(a.User); l <= 0 || l > maxLenUser {
+		return fmt.Errorf("invalid user: field must contain between 0 to %d characters, got %d", maxLenUser, l)
+	}
+
+	//The decoder appears to do _some_ validation when it can't map a string to the target type
+	//	For instance, it attempts to map the string "not a real time" to "2006-01-02T15:04:05Z07:00", but errors out with a "cannot parse ..." message
+	if a.EndTime.Before(a.StartTime) {
+		return fmt.Errorf("invalid end_time: end_time must occur after start_time")
+	}
+
+	if l := len(a.Description); l > maxLenDescription {
+		return fmt.Errorf("invalid description: length must not exceed %d characters, got %d", maxLenDescription, l)
+	}
+
+	//All checks passed
+	return nil
 }
 
 func processActivity(r io.Reader) error {
@@ -33,6 +57,11 @@ func processActivity(r io.Reader) error {
 		return err
 	}
 
+	err := act.Validate()
+
+	if err != nil {
+		return err
+	}
 	log.Printf("activity: %#v", act)
 	// TODO: Store in database
 
